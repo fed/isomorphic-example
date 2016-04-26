d102fe8a153ba84c2df5adbbc292d36c8a64541f
Federico Knüssel <fknussel@gmail.com>
2016-04-26 12:46:50 +1000

    Add styling

--- src/components/container.js
+++ src/components/container.js
@@ -8,7 +8,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="isomorphic-example">
         <h1>Isomorphic JavaScript Example</h1>
 
         <nav className="navigation">
